fc106e80a3fa3da8f8ca29a0d52ded0fa7429070
Philip Ardley <Philip Ardley>
2017-11-10 16:36:08 +0800

    Divided into unit tests

--- src/test/java/SinglyLinkedListTest.java
+++ src/test/java/SinglyLinkedListTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class SinglyLinkedListTest {
 
     SinglyLinkedList list;
@@ -15,39 +18,73 @@ public class SinglyLinkedListTest {
     }
 
     @Test
-    public void testAdditions() {
-
+    public void shouldAdd() {
         list.add(5);
-        assert(list.size() == 5);
+        assertThat(list.size(), equalTo(5));
+        assertThat(list.get(4), equalTo(0));
+    }
+
+    @Test
+    public void testAddToThirdPosition() {
+        list.add(3, 1);
+        System.out.println(list);
+        assert(list.contains(1));
+    }
+
+    @Test
+    public void testAddFirst() {
+        list.addFirst("First");
+        Object o = list.get(0);
+        assert o instanceof String && "First".equals(o);
+    }
+
+    @Test
+    public void testAddLast() {
+        list.addLast("Last");
+        Object o = list.get(list.size - 1);
+        assert o instanceof String && "Last".equals(o);
+    }
+
+    @Test
+    public void testSet() {
+        list.set(0, "set");
+        list.set(3, "set");
+        assert "set".equals(list.get(0));
+        assert"set".equals(list.get(3));
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testSetOutOfBounds() {
+        list.set(4, "set");
+    }
+
+    @Test
+    public void testRemove() {
+        list.removeFirst();
+        list.removeLast();
+        assert list.size == 2;
+        assert list.get(0).equals(3);
+        assert list.get(1).equals(2);
+    }
+
+    @Test
+    public void shouldRemoveAtIndex() {
+        list.remove(2);
+        assertThat(list.size, equalTo(3));
+        assertThat(list.get(2), equalTo(0));
     }
 
     public void mainTest() {
         setup();
         //testAdditions();
-
-        list.add(2, 1);
-        System.out.println(list);
-        System.out.println(list.contains(0));
-        System.out.println(list.contains(2));
-        System.out.println(list.contains(5));
-        System.out.println();
-
-        list.addFirst("First");
-        list.addLast("Last");
-        System.out.println(list);
-
-        list.set(0, "set");
-        list.set(7, "set");
-        System.out.println(list);
+//        testAddToThirdPosition();
+//        testAddFirst();
+//        testAddLast();
+//        testSet();
+//        testSetOutOfBounds();
+//        testRemove();
 
 
-        list.removeFirst();
-        list.removeLast();
-        System.out.println(list);
-
-
-        // Removes the item in the specified index
-        list.remove(2);
         System.out.println(list);
 
         // Removes the first occurrence of the specified object
@@ -75,8 +112,10 @@ public class SinglyLinkedListTest {
         System.out.println(list.get(2));
 
         list.clear();
-        System.out.println(list);
     }
+
+
+
     public static void main(String[] args) {
         new SinglyLinkedListTest().mainTest();
     }
